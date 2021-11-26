13c7559167f1a3f6caeebc93b8d2636697bf4a62
Sakamoto <daisuke814@outlook.com>
2021-11-26 21:35:58 +0900

    スケルトン

---
+++ tests/Unit/Controller/PageControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OCA\SakamotoApp\Tests\Unit\Controller;
+
+use PHPUnit_Framework_TestCase;
+
+use OCP\AppFramework\Http\TemplateResponse;
+
+use OCA\SakamotoApp\Controller\PageController;
+
+
+class PageControllerTest extends PHPUnit_Framework_TestCase {
+	private $controller;
+	private $userId = 'john';
+
+	public function setUp() {
+		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+
+		$this->controller = new PageController(
+			'sakamotoapp', $request, $this->userId
+		);
+	}
+
+	public function testIndex() {
+		$result = $this->controller->index();
+
+		$this->assertEquals('index', $result->getTemplateName());
+		$this->assertTrue($result instanceof TemplateResponse);
+	}
+
+}
